5ee9eb7f80bd4b101b234aedefea6b82d46d803b
ObsidianBlaze <wijojames@gmail.com>
2025-04-07 17:20:43 +0100

    feat: working on home card and card container component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
 import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
 </script>
 
 
 <template>
   <NavBar />
   <Hero />
+  <HomeCards />
 </template>
